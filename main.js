234fe0e175cba897cce5c248aa05df372c3ec8bb
CoderBoiiiiDebayan010 <78251765+CoderBoiiiiDebayan010@users.noreply.github.com>
2021-10-11 17:24:49 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,60 @@
+song = "";
+leftWristX = 0;
+leftWristY = 0;
+rightWristX = 0;
+rightWrisY = 0;
+
+function preload()
+{
+    song = loadSound("music.mp3");
+    song = loadSound("fnaf4.mp3");
+}
+
+
+
+function setup() {
+    canvas = createCanvas(730, 540);
+    canvas.center();
+
+    video = createCapture(VIDEO);
+    video.hide();
+
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses)
+}
+
+function gotPoses(results)
+{
+    if(results.length > 0)
+    {
+        console.log(results);
+        leftWristX = results[0].pose.leftWrist.x;
+        leftWristY = results[0].pose.leftWrist.y;
+        console.log("leftWristX = " + leftWristX +"leftWristY = "+ leftWristY);
+
+        rightWristX = results[0].pose.rightWrist.x;
+        rightWristY = results[0].pose.rightWrist.y;
+        console.log("rightWristX = " + rightWristX +"rightWristY = "+ rightWristY);
+    }
+
+}
+
+
+function modelLoaded() {
+    console.log('PoseNet Is Initialized! YEY!')
+}
+
+
+
+function draw() {
+    image(video, 0, 0, 730, 540);
+}
+
+
+
+function play()
+{
+    song.play();
+    song.setVolume(1);
+    song.rate(1);
+}
